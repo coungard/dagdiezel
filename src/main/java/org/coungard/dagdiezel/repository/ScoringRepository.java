3111d74da95778431ec8a1d5267e98953609e736
Artur.Mag <coungard@mail.ru>
2024-06-26 12:01:00 +0300

    fix generator, salah player

--- src/main/java/org/coungard/dagdiezel/repository/ScoringRepository.java
+++ src/main/java/org/coungard/dagdiezel/repository/ScoringRepository.java
@@ -3,15 +3,12 @@ package org.coungard.dagdiezel.repository;
 import java.util.List;
 import org.coungard.dagdiezel.entity.Scoring;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScoringRepository extends JpaRepository<Scoring, Long> {
 
-    List<Scoring> findAllByGameId(Long gameId);
-    List<Scoring> findAllByPlayerId(Long playerId);
+  List<Scoring> findAllByGameId(Long gameId);
 
-    @Query(value = "SELECT coalesce(max(id), 0) FROM Scoring ")
-    Long getMaxId();
+  List<Scoring> findAllByPlayerId(Long playerId);
 }
--- src/main/java/org/coungard/dagdiezel/tools/sequence/IncrementByMaxIdGenerator.java
+++ src/main/java/org/coungard/dagdiezel/tools/sequence/IncrementByMaxIdGenerator.java
@@ -8,18 +8,19 @@ import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.Configurable;
 import org.hibernate.id.IdentifierGenerator;
+import org.hibernate.persister.entity.EntityPersister;
 
 public class IncrementByMaxIdGenerator implements IdentifierGenerator, Configurable {
 
   @Override
   public Serializable generate(SharedSessionContractImplementor session, Object obj) throws HibernateException {
 
+    EntityPersister entityPersister = session.getEntityPersister(obj.getClass().getName(), obj);
     String query = String.format("select %s from %s",
-        session.getEntityPersister(obj.getClass().getName(), obj)
-            .getIdentifierPropertyName(),
-        obj.getClass().getSimpleName());
+        entityPersister.getIdentifierPropertyName(),
+        entityPersister.getPropertySpaces()[0]); // ? why spaces idk, need entity name , like a game/scoring
 
-    List<Long> ids = (List<Long>) session.createQuery(query).stream().collect(Collectors.toList());
+      List<Long> ids = (List<Long>) session.createQuery(query).stream().collect(Collectors.toList());
 
     return ids.isEmpty() ? 1 : Collections.max(ids) + 1;
   }
